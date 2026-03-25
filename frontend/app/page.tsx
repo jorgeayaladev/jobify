58ef368a934e93b0caebb59a0d66f716a8074186
Jorge Ayala <jayala42000@gmail.com>
2026-03-25 11:29:40 -0500

    version 1.0

---
+++ frontend/app/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  const [mensaje, setMensaje] = useState("");
+
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_API_URL);
+
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hola/`)
+      .then((res) => res.json())
+      .then((data) => setMensaje(data.message))
+      .catch((err) => console.error("ERROR:", err));
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className={styles.intro}>
+          <h1>Mensaje desde Django: {mensaje}</h1>
+          <p>
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className={styles.secondary}
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
